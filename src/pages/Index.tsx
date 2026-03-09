8ddd7cdcbc708d1edb52d915ca111dcdbdc5f0a7
Chaitanya Reddy <chaitanyareddy4683@gmail.com>
2026-03-09 03:32:08 +0000

    Preceding changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect, useCallback } from "react";
 import HeroSection from "@/components/HeroSection";
 import CuisineFilter from "@/components/CuisineFilter";
 import RestaurantCard from "@/components/RestaurantCard";
